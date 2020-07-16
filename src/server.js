7673d5392487e0746d5108d0619d956ee9ad4134
zitrot <mortiza3@eafit.edu.co>
2020-07-15 22:46:22 -0500

    fsd6

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/ANGULAR10-AFRAME'));
+app.use(express.static(__dirname + '/dist/Angular10-AFRAME'));
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/ANGULAR10-AFRAME/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/Angular10-AFRAME/index.html'));
 });
