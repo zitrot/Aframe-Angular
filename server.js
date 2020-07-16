d6c5814c2abe36ea714306b994fd9adda82ce804
zitrot <mortiza3@eafit.edu.co>
2020-07-15 23:29:14 -0500

    fsd9

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/Angular10-AFRAME'));
-app.get('/', function(req, res) {
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/Angular10-AFRAME'));
+
+app.get('/*', function(req, res) {
+
     res.sendFile(path.join(__dirname + '/dist/Angular10-AFRAME/index.html'));
 });
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
